d17347fb7dc75b49e09e5d450398ab19f9de7459
riponmondalbd <riponmondal149@gmail.com>
2024-12-09 18:23:51 +0600

    complete error page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,28 @@
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold font-bebas-neue">Oops!</h1>
+          <p className="py-4 font-montserrat">
+            Sorry, an unexpected error has occurred.
+          </p>
+          <p className="pb-6 font-montserrat">
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <Link to={-1}>
+            <button className="btn bg-[#F9A51A] font-montserrat">
+              Go Back
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
